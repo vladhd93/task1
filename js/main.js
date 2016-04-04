799ad82a523235cc32442ec4b4d1e11ed636c000
vladhd93 <vladhd@ukr.net>
2016-04-04 22:50:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//= components/toggleSlide.js
+
+var SlidePanel = new SlidePanel({
+    panel: document.querySelector("[data-component='slide-panel']"),
+    button: document.querySelector("[data-component='trigger-panel-btn']")
+});
+
